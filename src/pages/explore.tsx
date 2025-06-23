58c73d6c5053e9494b485adfb5d20d9fc545b67f
loew-tech <loew.technology@gmail.com>
2025-06-23 16:22:50 -0600

    kevinBacon with director and movie (id)
    need to add info on percentage of graph explored and change to use movie title instead of id - after BE chagne

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -40,11 +40,20 @@ const CREATOR_OPTIONS = [
   },
 ];
 
+const DEPTH_OPTIONS = Array.from({ length: 10 }, (_, i) => ({
+  key: i + 1,
+  text: `${i + 1}`,
+  value: i + 1,
+}));
+
 export const Explore = () => {
   const data = localStorage.getItem("user");
   const user = data ? (JSON.parse(data) as Member) : null;
 
   const [creator, setCreator] = useState<string>("");
+  const [director, setDirector] = useState<string>("");
+  const [movieTitle, setMovieTitle] = useState("");
+  const [depth, setDepth] = useState<number>(1);
   const [exploreType, setExploreType] = useState<string>("");
   const [starData, setStarData] = useState<string[] | null>(null);
   const [movieData, setMovieData] = useState<Movie[] | null>(null);
@@ -54,11 +63,22 @@ export const Explore = () => {
     _: React.SyntheticEvent<HTMLElement>,
     { value }: DropdownProps
   ) => {
+    if (value === KEVIN_BACON) {
+      setCreator("");
+    }
     setExploreType(value as string);
+  };
+
+  const handleDepthSelectionChange = async (
+    _: React.SyntheticEvent<HTMLElement>,
+    { value }: DropdownProps
+  ) => {
+    setDepth(value as number);
   };
 
   // @TODO: add loader?
   const explore = async () => {
+    console.log("exploreType=", exploreType);
     let stars: string[] | null = null;
     let movies: Movie[] | null = null;
     switch (exploreType) {
@@ -71,7 +91,7 @@ export const Explore = () => {
         movies = await directedMovies(creator);
         break;
       case KEVIN_BACON:
-        const kb = await kevinBacon(creator);
+        const kb = await kevinBacon(creator, movieTitle, director, depth);
         setStarData(kb?.stars ?? null);
         setMovieData(kb?.movies ?? null);
         setDirectorData(kb?.directors ?? null);
@@ -87,14 +107,15 @@ export const Explore = () => {
     <>
       <HeaderBanner user={user} />
       <Container text className="movie-container">
-        <div style={{ display: "flex" }}>
-          <Dropdown
-            placeholder="Select Search"
-            fluid
-            selection
-            options={CREATOR_OPTIONS}
-            onChange={handleSelectionChange}
-          />
+        <Dropdown
+          placeholder="Select Search"
+          fluid
+          selection
+          options={CREATOR_OPTIONS}
+          onChange={handleSelectionChange}
+        />
+        {exploreType !== KEVIN_BACON ? (
+          // @TODO: add css for this file
           <div className="ui focus input">
             <input
               type="text"
@@ -103,8 +124,45 @@ export const Explore = () => {
               onChange={(e) => setCreator(e.target.value)}
             />
           </div>
-          <Button onClick={explore}>Go!</Button>
-        </div>
+        ) : (
+          <div>
+            <div className="ui focus input">
+              Star:
+              <input
+                type="text"
+                placeholder="Enter STAR Name"
+                value={creator}
+                onChange={(e) => setCreator(e.target.value)}
+              />
+            </div>
+            <div className="ui focus input">
+              Movie
+              <input
+                type="text"
+                placeholder="Enter Movie Title"
+                value={movieTitle}
+                onChange={(e) => setMovieTitle(e.target.value)}
+              />
+            </div>
+            <div className="ui focus input">
+              DIRECTOR:
+              <input
+                type="text"
+                placeholder="Enter Director Name"
+                value={director}
+                onChange={(e) => setDirector(e.target.value)}
+              />
+            </div>
+            <Dropdown
+              placeholder="Select SEarch Depth"
+              fluid
+              selection
+              options={DEPTH_OPTIONS}
+              onChange={handleDepthSelectionChange}
+            />
+          </div>
+        )}
+        <Button onClick={explore}>Go!</Button>
       </Container>
       {starData ? (
         // @TODO: style this properly
