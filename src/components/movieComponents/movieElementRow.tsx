62979f13504bc3a00709133cc6b1ac64c79021de
loew-tech <loew.technology@gmail.com>
2025-07-05 13:55:55 -0600

    refactors to movie

--- src/components/movieComponents/movieElementRow.tsx
+++ src/components/movieComponents/movieElementRow.tsx
@@ -4,13 +4,15 @@ import { GridColumn, GridRow } from "semantic-ui-react";
 type MoevieElementRowProps = {
   sectionTitle: string;
   content: ReactNode;
+  key: string;
 };
 export const MovieElementRow = ({
   sectionTitle,
   content,
+  key,
 }: MoevieElementRowProps) => {
   return (
-    <GridRow>
+    <GridRow key={key}>
       <GridColumn>
         <h3>{sectionTitle}</h3>
       </GridColumn>
